ad644af25129e7acf954b195aca9eaf2d8d93daa
fabien CCDC <fcolard9@gmail.com>
2021-10-12 17:52:26 +0200

    add random color boxes

--- src/app/components/box/box.component.ts
+++ src/app/components/box/box.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
   styleUrls: ['./box.component.scss']
 })
-export class BoxComponent implements OnInit {
+export class BoxComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() color: string = '';
+  colorText: string = '#FFFFFF';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+  //onEventNewColor =>
+  ngOnChanges(changes: any): void {
+    this.color = changes.color.currentValue;
+    this.colorText = this.colorOfText(this.color);
+  }
 
+  valueRGB(c: string): number {
+    let value = 0;
+    if (c.length === 1) {
+      switch (c.toLowerCase()) {
+        case 'a': value = 10; break;
+        case 'b': value = 11; break;
+        case 'c': value = 12; break;
+        case 'd': value = 13; break;
+        case 'e': value = 14; break;
+        case 'f': value = 15; break;
+        default: value = parseInt(c); break;
+      }
+    }
+    return value;
+  }
+
+  colorOfText(color: string): string {
+    //mini algo maison
+    if (color.length === 7) { //vrai
+      let colorSum: number = this.valueRGB(color[1]) + this.valueRGB(color[3]) + this.valueRGB(color[5]);
+      if ((colorSum - 22) < 0) return '#FFFFFF';
+    }
+    return '#000000';
+  }
 }
